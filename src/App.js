164f89b23b27e5f052caefd34096cacb4ed3a5ca
Aswin <aswinjikz071@gmail.com>
2023-11-13 12:25:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import WeatherApp from './componets/WeatherScreen/WeatherApp';
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp />
+    </div>
+  );
+}
+
+export default App;
